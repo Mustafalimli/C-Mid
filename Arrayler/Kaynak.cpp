f4d5800cb39e6174d0d491763ada8226c58d1622
Mustafa Alimli <101422269+Mustafalimli@users.noreply.github.com>
2024-11-04 23:35:53 +0300

    Add files via upload

---
+++ Arrayler/Kaynak.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int i;
+	int array[3];
+	array[0] = 10;
+	array[1] = 20;
+	array[2] = 30;
+
+	string array1[4];
+
+	for (i = 0; i < 4; i++) {
+
+		cin >> array[4];
+	}
+
+	for (i = 0; i < 4; i++) {
+
+		cout << i << ". Degeri:" << array1[i] << endl;
+
+	}
+
+
+	return 0;
+}
