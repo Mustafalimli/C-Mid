f8593c4a508abe8b9d7a56fd31a6d54ef2d6385e
Mustafa Alimli <101422269+Mustafalimli@users.noreply.github.com>
2024-11-16 23:42:30 +0300

    Add files via upload

---
+++ boşluklu diktörtgen.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	
+	int i,j;
+	
+	for(i=0;i<5;i++){
+		
+		for(j=0;i<j;j++){
+			
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ dik üçgen.cpp
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	
+	float a,b,c;
+	float d,e,f;
+	float n,m,k;
+	
+	
+	printf("1.Kenari Giriniz:");
+	scanf("%f",&a);
+	
+	printf("2.Kenari Giriniz:");
+	scanf("%f",&b);
+	
+	printf("3.Kenari Giriniz:");
+	scanf("%f",&c);
+	
+
+	d = abs(a-b);
+	e = abs(a-c);
+	f = abs(a-f);
+	n = a*a;
+	m = b*b;
+	k = c*c;
+	
+		
+
+	     if (n+m==k){
+		printf ("Dik Ucgen Olur");
+		
+		}
+		else ("Dik Ucgen Olmaz:");
+		
+		
+	
+	
+	return 0;
+}
